fcf10765dd9be148d2f468456e86493d7a4f795e
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-02-10 19:05:34 +0800

    Chore: Remove debugging lines

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // A target is the Cell() that a player marks during his move
-// A player's mark is either X or O for tic-tac
+// A player's mark is either X or O for tic-tac-toe
 // Each player should input a row and column to determine where to input his mark
 function Board() {
     const columns = 3;
@@ -18,6 +18,7 @@ function Board() {
             board[i].push(Cell());
         }
     }
+
     const getMove = () => move;
     const getWinner = () => winner;
 
@@ -26,11 +27,22 @@ function Board() {
         // check the mark on the checkBoard against the mark of the players
         players.forEach((participant) => {
             if (participant.mark === boardStatus.playerMark) {
-                console.log(`setWinner(): What is the current mark? ${boardStatus.playerMark} and whose is it? ${participant.name}`)
                 winner = participant;
             }
         })
 
+    }
+    const createDOMBoard = () => {
+        const gameBoard = document.querySelector(".board");
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                const cell = document.createElement("div");
+                cell.setAttribute("class", `cell row${i} col${j}`);
+                cell.textContent = "0";
+                gameBoard.appendChild(cell);
+            }
+        }
+    
     }
 
     const printBoard = () => {
@@ -39,18 +51,16 @@ function Board() {
     }
 
     function putMove(row, column, playerMark) {
+        let targetDOMCell = document.querySelector(`.row${row}.col${column}`)
         let targetCell = board[row][column];
-        // console.log(`targetCell is ${targetCell.}`)
         // Cells with 0 are unfilled, otherwise they're already marked
+        // only check the board if move is valid;
         if (targetCell.getMove() === 0) {
-            console.log(`putMove(): move at targetCell is ${targetCell.getMove()}`)
+            targetDOMCell.textContent = playerMark;
             targetCell.setMove(playerMark);
-            console.log(`putMove(): new move at targetCell is ${targetCell.getMove()}`)
             move.isValid = true;
-            console.log(`putMove(): Is the move valid? ${move.isValid}`)
-            // only check the board if move is valid;
             boardStatus = checkBoard(row, column, playerMark);
-            // if a patter is found, set the winner
+            // if a pattern is found, set the winner
             if (boardStatus.getResult())
                 setWinner();
         } else {
@@ -60,47 +70,35 @@ function Board() {
     }
 
     const checkBoard = (row, column, playerMark) => {
-        console.log(`Board is being checked...`)
         let currentRow = row === true ? 1 : row;
         let currentCol = column;
         let currentMark = playerMark;
         let patternFound, verticalCheck, horizontalCheck;
         let diagonalCheck = false;
 
-        console.log(`Current row and col is ${currentRow} ${currentCol}`)
         const checkHorizontally = () => {
             currentRow = currentRow === true ? 1 : currentRow;
-            console.log(`Checking Horizontally...`)
             for (let col = 0; col < 3; col++) {
-                console.log(`Is move ${board[currentRow][col].getMove()} not equal to ${currentMark}?`)
                 if (board[currentRow][col].getMove() != currentMark) {
-                    console.log(`A horizontal mismatch is found. Aborting checking.`);
                     return false
                 }
             }
-            console.log(`Horizontal Pattern found at checkHorizontally()`);
             return true
         }
         const checkVertically = () => {
-            console.log(`Checking Vertically...`)
             for (let row = 0; row < 3; row++) {
-                console.log(`Is move ${board[row][currentCol].getMove()} not equal to ${currentMark}?`)
                 if (board[row][currentCol].getMove() != currentMark) {
-                    console.log(`A vertical mismatch is found. Aborting checking.`);
                     return false
                 }
             }
-            console.log(`Vertical Pattern found at checkHorizontally()`);
             return true
         }
+
         const checkDiagonally = () => {
-            console.log(`Checking Diagonally...`)
             if (currentRow === 1 && currentCol === 1) {
-                console.log(`Is it at the center? ${currentRow = currentCol === 1}`)
                 return check(isCenter = true)
             }
             else if (currentRow + currentCol === 2 || currentRow === currentCol) {
-                console.log(`Is it not at the center? ${currentRow + currentCol === 2 || currentRow === currentCol}`)
                 return check(isCenter = false)
             }
             else {
@@ -108,18 +106,21 @@ function Board() {
             }
 
             function check(isCenter = false) {
+                /*
+                * Indexes for Diagonal patterns
+                * (0, 0) \                                  / (0, 2)
+                *       (1, 1) \                      / (1, 1)
+                *              (2, 2) \    OR   /(2, 0)
+                */
                 let rowIterations = currentRow === 2 ? [2, 1, 0] : [0, 1, 2]
                 let colIterations = currentCol === 2 ? [2, 1, 0] : [0, 1, 2]
                 let index = 0;
                 let patternFound = true;
 
-                console.log(`First Diagonal Check`)
                 while (index < 3) {
-
                     let rowIndex = rowIterations[index]
                     let colIndex = colIterations[index]
                     if (board[rowIndex][colIndex].getMove() != currentMark) {
-                        console.log(`A diagonal mismatch is found at the first diagonal check. Abort checking`)
                         patternFound = false
                         break;
                     }
@@ -127,21 +128,17 @@ function Board() {
                 };
 
                 if (isCenter) {
-                    console.log(`Second Diagonal Check`)
                     let index = 2
                     while (index >= 0) {
                         let rowIndex = rowIterations[index]
                         let colIndex = colIterations[index]
                         if (board[rowIndex][colIndex].getMove() != currentMark) {
-                            console.log(`A diagonal mismatch is found at the second diagonal check. Abort checking`)
                             patternFound = false
                             break;
                         }
                         index--;
-
                     }
                 }
-                // console.log(`Diagonal Pattern found at checkDiagonally()`)
                 return patternFound;
             }
         }
@@ -151,13 +148,12 @@ function Board() {
         horizontalCheck = checkHorizontally();
         const getResult = () => {
             patternFound = verticalCheck || horizontalCheck || (diagonalCheck === false ? false : true)
-            console.log(`PatternFound: ${patternFound}`)
             return patternFound;
         };
-        console.log(`Vertical Match? : ${verticalCheck}, Horizontal Match? ${horizontalCheck}, Diagonal Match? ${diagonalCheck}`)
+
         return { getResult, playerMark }
     }
-    return { printBoard, putMove, getWinner, getMove }
+    return { printBoard, putMove, getWinner, getMove, createDOMBoard }
 }
 
 function Cell() {
@@ -205,7 +201,6 @@ function GamePlay() {
 
     const startNewRound = () => {
         board.printBoard();
-
         console.log(`It is ${getActivePlayer().name}'s turn!`)
     }
 
@@ -214,11 +209,8 @@ function GamePlay() {
         board.putMove(row, column, getActivePlayer().mark)
 
         const winner = board.getWinner();
-        console.log(winner)
         const move = board.getMove();
-        console.log(`winner undefined? ${winner === undefined}, turn < 10? ${turn < 10}, valid move? ${move.isValid}`)
-        console.log(`${winner === undefined || (turn < 10 && move.isValid)}`)
-        console.log();
+
         if (winner === undefined && turn < 10 && move.isValid) {
             turn++;
             switchActivePlayer();
@@ -233,6 +225,7 @@ function GamePlay() {
     }
 
     startNewRound();
+    board.createDOMBoard();
     return { playRound, getActivePlayer }
 }
 const game1 = GamePlay();
@@ -244,24 +237,6 @@ game1.playRound(2, 2);
 game1.playRound(1, 1);
 game1.playRound(2, 0);
 game1.playRound(2, 1);
-// game1.playRound(2, 0);
-// game1.playRound(0, 2);
-
-// const board = Board();
-// console.log(board.board);
-// board.printBoard()
-
-// const move1 = Cell();
-// move1.setMove("X")
-// console.log(move1.getMove());
-
-// let players = Players();
-// console.log(players.players[0].name)
-// console.log(players.players[0].mark)
-
-// if row === column  || row + column === 2
-// row === 1
-// checkDiagonally()
 
 // at row=0, column=0
 // check horizontally at row 0, from column 0 - 2
